ba4d48fdac4205ec5c5a1649a2afcbab3b4ac843
Victor <hello@victordias.dev>
2022-06-30 00:47:37 -0300

    initialize the structuring of the protocol rules

--- server.c
+++ server.c
@@ -10,11 +10,39 @@
 
 #define INPUT_ARGS 2
 #define MAX_THREADS 15
+#define MAX_CLIENTS 15
 #define MAX_PENDING 5
 #define BUFSIZE 500
+#define EQUIPMENT_NONE 0
+
+enum
+{
+    REQ_ADD_ID = 1,
+    REQ_REM_ID,
+    RES_ADD_ID,
+    RES_LIST_ID,
+    REQ_INF_ID,
+    RES_INF_ID,
+    ERROR_ID,
+    OK_ID,
+};
+
+enum
+{
+    EQUIPMENT_NOT_FOUND = 1,
+    SOURCE_EQUIPMENT_NOT_FOUND,
+    TARGET_EQUIPMENT_NOT_FOUND,
+    EQUIPMENT_LIMIT_EXCEEDED,
+};
+
+enum
+{
+    SUCCESFUL_REMOVAL = 1
+};
 
 int serverSock;
 int numberOfThreads = 0;
+int numberOfClients = 0;
 
 void *ThreadMain(void *arg);
 
@@ -23,6 +51,14 @@ struct ThreadArgs
     socklen_t clientLen;
     struct sockaddr_in clientCon;
     char buffer[BUFSIZE];
+};
+
+struct Message
+{
+    int IdMsg;
+    int IdOrigin;
+    int IdDestination;
+    int Payload;
 };
 
 int buildServerSocket(char *portString)
@@ -61,17 +97,95 @@ int buildServerSocket(char *portString)
     return sock;
 }
 
+void assembleMessage(char *buffer, struct Message *message)
+{
+    char finalMessage[BUFSIZE] = "";
+    char partialMessage[BUFSIZE] = "";
+    if (message->IdMsg)
+    {
+        sprintf(partialMessage, "%d ", message->IdMsg);
+        strcat(finalMessage, partialMessage);
+    }
+    if (message->IdOrigin)
+    {
+        sprintf(partialMessage, "%d ", message->IdOrigin);
+        strcat(finalMessage, partialMessage);
+    }
+    if (message->IdDestination)
+    {
+        sprintf(partialMessage, "%d ", message->IdDestination);
+        strcat(finalMessage, partialMessage);
+    }
+    if (message->Payload)
+    {
+        sprintf(partialMessage, "%d", message->Payload);
+        strcat(finalMessage, partialMessage);
+    }
+    strcpy(buffer, finalMessage);
+}
+
+void buildERROR(char *buffer, int idDestination, int payload)
+{
+    struct Message message;
+    message.IdMsg = ERROR_ID;
+    message.IdOrigin = EQUIPMENT_NONE;
+    message.IdDestination = idDestination;
+    message.Payload = payload;
+    assembleMessage(buffer, &message);
+}
+
+void buildRESADD(char *buffer)
+{
+    struct Message message;
+    message.IdMsg = RES_ADD_ID;
+    message.IdOrigin = EQUIPMENT_NONE;
+    message.IdDestination = EQUIPMENT_NONE;
+    message.Payload = numberOfClients;
+    assembleMessage(buffer, &message);
+}
+
+int IdentifyMessage(char *buffer)
+{
+    char *command = strtok(buffer, " ");
+    int id = atoi(command);
+    return id;
+}
+
+void AddEquipment(char *response)
+{
+    buildRESADD(response);
+}
+
 void *ThreadMain(void *args)
 {
     struct ThreadArgs *threadArgs = (struct ThreadArgs *)args;
+    int idMessage = IdentifyMessage(threadArgs->buffer);
+    char response[BUFSIZE] = "";
+    switch (idMessage)
+    {
+    case REQ_ADD_ID:
+        numberOfClients++;
+        AddEquipment(response);
+        break;
+    case REQ_REM_ID:
+        numberOfClients--;
+        // RemoveEquipment(threadArgs->buffer);
+        break;
+    case REQ_INF_ID:
+        // GetEquipmentInfo(threadArgs->buffer);
+        break;
+    }
+
     char *message = threadArgs->buffer;
     int connection_id = threadArgs->clientCon.sin_port;
     struct sockaddr_in from = threadArgs->clientCon;
     char *ip = inet_ntoa(from.sin_addr);
     printf("%s:%d: %s\n", ip, connection_id, message);
-    char *response = "OK\n";
     int bytesSent = sendto(serverSock, response, strlen(response), 0, (struct sockaddr *)&threadArgs->clientCon, threadArgs->clientLen);
     validateCommunication(bytesSent);
+
+    free(threadArgs);
+    numberOfThreads--;
     return NULL;
 }
 
@@ -89,6 +203,16 @@ int main(int argc, char const *argv[])
         threadArgs->clientLen = sizeof(struct sockaddr_in);
         int bytesReceived = recvfrom(serverSock, threadArgs->buffer, BUFSIZE, 0, (struct sockaddr *)&threadArgs->clientCon, &threadArgs->clientLen);
         validateCommunication(bytesReceived);
+        printf("numberOfClients: %d\n", numberOfClients);
+        printf("%s\n", threadArgs->buffer);
+        if (numberOfClients == MAX_CLIENTS)
+        {
+            char response[BUFFER_SIZE_BYTES] = "";
+            buildERROR(response, EQUIPMENT_NONE, EQUIPMENT_LIMIT_EXCEEDED);
+            int bytesSent = sendto(serverSock, response, strlen(response), 0, (struct sockaddr *)&threadArgs->clientCon, threadArgs->clientLen);
+            validateCommunication(bytesSent);
+            continue;
+        }
 
         int threadStatus = pthread_create(&threads[numberOfThreads], NULL, ThreadMain, (void *)threadArgs);
         if (threadStatus != 0)
