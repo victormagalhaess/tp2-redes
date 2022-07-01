515614192b820d4538c7f8ae688f4b7609cb8b9b
Victor <hello@victordias.dev>
2022-07-01 00:14:42 -0300

    initialize client implementation

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ EXEC_SERVER=./server
 all: $(EXEC_EQUIPMENT) $(EXEC_SERVER)
 
 $(EXEC_EQUIPMENT): equipment.c common.o
-	$(CC) $(CFLAGS) equipment.c common.o -o $(EXEC_EQUIPMENT)
+	$(CC) $(CFLAGS) $(THREADFLAG) equipment.c common.o -o $(EXEC_EQUIPMENT)
 
 $(EXEC_SERVER): server.c common.o
 	$(CC) $(CFLAGS) $(THREADFLAG) server.c common.o -o $(EXEC_SERVER)
--- equipment.c
+++ equipment.c
@@ -3,11 +3,16 @@
 #include "common.h"
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <time.h>
 
-/* This section is responsible to just read the commands from the console.*/
+#define BUFSIZE 500
+
+int clientSock;
+socklen_t clientLen = sizeof(struct sockaddr_in);
 
 void readMessage(char *message)
 {
@@ -16,76 +21,27 @@ void readMessage(char *message)
     return;
 }
 
-/* All the socket creation logic is segregated in the following function.
-It receives the ip of the server (expected to be an valid ipv4 or ipv6) and the port,
-builds the socket and returns the int that represents the socket connection */
-
-int buildClientSocket(int argc, char const *argv[])
-{
-    validateInputArgs(argc, 3);
-    int sock = 0;
-    int domain;
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in6 serv_addrv6;
-
-    if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) > 0)
-    {
-        domain = AF_INET;
-    }
-
-    if (inet_pton(AF_INET6, argv[1], &serv_addrv6.sin6_addr) > 0)
-    {
-        domain = AF_INET6;
-    }
-
-    int port = getPort(strdup(argv[2]));
-
-    if ((sock = socket(domain, SOCK_STREAM, IPPROTO_TCP)) < 0)
-    {
-        dieWithMessage("Socket creation error\n");
-    }
-
-    serv_addr.sin_family = domain;
-    serv_addr.sin_port = htons(port);
-
-    serv_addrv6.sin6_family = domain;
-    serv_addrv6.sin6_port = htons(port);
-
-    struct sockaddr *conAddress = (struct sockaddr *)&serv_addr;
-    int conSize = sizeof(serv_addr);
-    if (domain == AF_INET6)
-    {
-        conAddress = (struct sockaddr *)&serv_addrv6;
-        conSize = sizeof(serv_addrv6);
-    }
-
-    if (connect(sock, conAddress, conSize) < 0)
-    {
-        dieWithMessage("Connection Failed \n");
-    }
-
-    return sock;
-}
-
-/*The last section covers the main function that is responsible to connect to the server,
-nitialize variables, and start the reading loop, where the client enters in a state that it waits the terminal inputs to
-send them as a message to the server. Then it awaits the server to and execute the validcommands that it receives and
-to return an feedback.*/
-
 int main(int argc, char const *argv[])
 {
-    int sock = buildClientSocket(argc, argv);
+    validateInputArgs(argc, 3);
+    char *serverIP = strdup(argv[1]);
+    char *serverPort = strdup(argv[2]);
+    int serverPortNumber = getPort(serverPort);
+    clientSock = buildUDPSocket("0");
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(serverPortNumber);
+    serverAddr.sin_addr.s_addr = inet_addr(serverIP);
     char buffer[BUFFER_SIZE_BYTES] = {0};
-    char message[BUFFER_SIZE_BYTES];
 
     for (;;)
     {
-        readMessage(message);
-        int totalBytesSent = send(sock, message, strlen(message), 0);
+        readMessage(buffer);
+        int totalBytesSent = sendUdpMessage(clientSock, buffer, &serverAddr);
         validateCommunication(totalBytesSent);
         memset(buffer, 0, sizeof(buffer));
-        int totalBytesRead = read(sock, buffer, BUFFER_SIZE_BYTES);
-        validateCommunication(totalBytesRead);
+        int bytesReceived = recvfrom(clientSock, buffer, BUFSIZE, 0, (struct sockaddr *)&serverAddr, &clientLen);
+        validateCommunication(bytesReceived);
         printf("%s\n", buffer);
     }
     return 0; // never reached
